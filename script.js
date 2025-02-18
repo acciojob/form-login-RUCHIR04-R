f3ad1743085478f1bb5a25a68998016db8a6ddf5
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-18 10:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+// script.js
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form from submitting the traditional way
+  getFormvalue();
+});
 
+function getFormvalue() {
+  const firstName = document.querySelector('input[name="fname"]').value.trim();
+  const lastName = document.querySelector('input[name="lname"]').value.trim();
+
+  if (firstName && lastName) {
+    alert(`${firstName} ${lastName}`);
+  } else {
+    alert('Please enter both first name and last name.');
+  }
 }
